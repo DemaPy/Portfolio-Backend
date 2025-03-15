640d2f31841d67f37683ce63a84845e698464adf
DemaPy <benderdema@gmail.com>
2025-03-15 23:38:11 +0100

    add env origin

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     cors: {
       methods: ['GET', 'POST'],
-      origin:
-        process.env.MODE === 'DEV'
-          ? 'http://localhost:5173'
-          : 'https://demchenko.uk',
+      origin: process.env.ORIGIN,
     },
   });
   await app.listen(process.env.PORT ?? 3000);
