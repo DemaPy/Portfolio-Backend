1b4b487a97bf235b49606a066d59afd73cd237ce
DemaPy <benderdema@gmail.com>
2025-03-15 23:36:05 +0100

    fix mode env

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     cors: {
       methods: ['GET', 'POST'],
-      origin: process.env.DEV
-        ? 'http://localhost:5173'
-        : 'http://localhost:5173',
+      origin:
+        process.env.MODE === 'DEV'
+          ? 'http://localhost:5173'
+          : 'https://demchenko.uk',
     },
   });
   await app.listen(process.env.PORT ?? 3000);
